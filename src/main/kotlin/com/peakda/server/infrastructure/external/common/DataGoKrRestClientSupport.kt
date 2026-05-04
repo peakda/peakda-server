be34777ce5e840e0414dfd0ab68638e56227b511
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-05-04 13:42:28 +0900

    feat: 외부 API 공통 RestClient 팩토리/지원 추가

---
+++ src/main/kotlin/com/peakda/server/infrastructure/external/common/DataGoKrRestClientSupport.kt
@@ -0,0 +1,34 @@
+package com.peakda.server.infrastructure.external.common
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
+
+inline fun <reified T : Any> RestClient.getDataGoKrBody(
+    objectMapper: ObjectMapper,
+    errorDecoder: DataGoKrErrorDecoder,
+    path: String,
+    queryParams: Map<String, Any?> = emptyMap(),
+): DataGoKrBody<T> {
+    val rawBody = get()
+        .uri { builder ->
+            builder.path(path)
+            queryParams.forEach { (name, value) ->
+                when (value) {
+                    null -> Unit
+                    is Iterable<*> -> value.filterNotNull().forEach { builder.queryParam(name, it) }
+                    else -> builder.queryParam(name, value)
+                }
+            }
+            builder.build()
+        }
+        .retrieve()
+        .body<String>()
+        .orEmpty()
+
+    errorDecoder.throwIfXmlError(rawBody)
+
+    val envelope = objectMapper.readValue<DataGoKrEnvelope<T>>(rawBody)
+    return errorDecoder.decode(envelope)
+}
