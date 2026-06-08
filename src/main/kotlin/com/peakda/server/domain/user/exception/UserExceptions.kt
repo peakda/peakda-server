8a3cf95716c166c99629287e278540edd956dfd4
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-06-08 12:41:31 +0900

    feat: 자기 자신 팔로우 금지 에러코드와 예외 추가

--- src/main/kotlin/com/peakda/server/domain/user/exception/UserExceptions.kt
+++ src/main/kotlin/com/peakda/server/domain/user/exception/UserExceptions.kt
@@ -6,3 +6,5 @@ import com.peakda.server.common.exception.ErrorCode
 class NicknameDuplicatedException : BusinessException(ErrorCode.NICKNAME_DUPLICATED)
 
 class UserNotFoundException : BusinessException(ErrorCode.RESOURCE_NOT_FOUND)
+
+class SelfFollowNotAllowedException : BusinessException(ErrorCode.SELF_FOLLOW_NOT_ALLOWED)
